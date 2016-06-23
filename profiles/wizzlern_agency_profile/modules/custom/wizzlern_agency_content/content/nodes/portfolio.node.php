6180e613c47a2495182a98507aebf2c888b7e8bc
Erik Stielstra <info@erikstielstra.nl>
2016-06-23 13:53:28 +0200

    Fix text format for portfolio content

--- profiles/wizzlern_agency_profile/modules/custom/wizzlern_agency_content/content/nodes/portfolio.node.php
+++ profiles/wizzlern_agency_profile/modules/custom/wizzlern_agency_content/content/nodes/portfolio.node.php
@@ -13,9 +13,12 @@ $nodes[] = array(
   'promote' => 0,
   'sticky' => 0,
   'type' => 'portfolio',
-  'body' => '<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+  'body' => [
+    'value' => '<p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
 <p><stong>Want these icons in this portfolio item sample?<stong> You can download 60 of them for free, courtesy of RoundIcons.com, or you can purchase the 1500 icon set here.</p>
 <p>Date: July 2014 Client: Round Icons Category: Graphic Design</p>',
+    'format' => 'basic_html'
+  ],
   'field__subtitle' => 'Lorem ipsum dolor sit amet consectetur.',
   'field_portfolio_screenshot' => $files['portfolio/roundicons.png'],
   'field_portfolio_date' => 'July 2014',
@@ -31,8 +34,11 @@ $nodes[] = array(
   'promote' => 0,
   'sticky' => 0,
   'type' => 'portfolio',
-  'body' => '<p>Startup Framework is a website builder for professionals. Startup Framework contains components and complex blocks (PSD+HTML Bootstrap themes and templates) which can easily be integrated into almost any design. All of these components are made in the same style, and can easily be integrated into projects, allowing you to create hundreds of solutions for your future projects.</p>
+  'body' => [
+    'value' => '<p>Startup Framework is a website builder for professionals. Startup Framework contains components and complex blocks (PSD+HTML Bootstrap themes and templates) which can easily be integrated into almost any design. All of these components are made in the same style, and can easily be integrated into projects, allowing you to create hundreds of solutions for your future projects.</p>
 <p>You can preview Startup Framework here.</p>',
+    'format' => 'basic_html'
+  ],
   'field__subtitle' => 'Lorem ipsum dolor sit amet consectetur.',
   'field_portfolio_screenshot' => $files['portfolio/startup-framework.png'],
   'field_portfolio_date' => 'January 2014',
@@ -47,8 +53,11 @@ $nodes[] = array(
   'promote' => 0,
   'sticky' => 0,
   'type' => 'portfolio',
-  'body' => '<p>Treehouse is a free PSD web template built by Mathavan Jaya. This is bright and spacious design perfect for people or startup companies looking to showcase their apps or other projects.</p>
+  'body' => [
+    'value' => '<p>Treehouse is a free PSD web template built by Mathavan Jaya. This is bright and spacious design perfect for people or startup companies looking to showcase their apps or other projects.</p>
 <p>You can download the PSD template in this portfolio sample item at FreebiesXpress.com.</p>',
+    'format' => 'basic_html'
+  ],
   'field__subtitle' => 'Lorem ipsum dolor sit amet consectetur.',
   'field_portfolio_screenshot' => $files['portfolio/treehouse.png'],
   'field_portfolio_date' => 'November 2013',
@@ -64,8 +73,11 @@ $nodes[] = array(
   'promote' => 0,
   'sticky' => 0,
   'type' => 'portfolio',
-  'body' => "<p>Start Bootstrap's Agency theme is based on Golden, a free PSD website template built by Mathavan Jaya. Golden is a modern and clean one page web template that was made exclusively for Best PSD Freebies. This template has a great portfolio, timeline, and meet your team sections that can be easily modified to fit your needs.</p>
+  'body' => [
+    'value' => "<p>Start Bootstrap's Agency theme is based on Golden, a free PSD website template built by Mathavan Jaya. Golden is a modern and clean one page web template that was made exclusively for Best PSD Freebies. This template has a great portfolio, timeline, and meet your team sections that can be easily modified to fit your needs.</p>
 <p>You can download the PSD template in this portfolio sample item at FreebiesXpress.com.</p>",
+    'format' => 'basic_html'
+  ],
   'field__subtitle' => 'Lorem ipsum dolor sit amet consectetur.',
   'field_portfolio_screenshot' => $files['portfolio/golden.png'],
   'field_portfolio_date' => 'October 2013',
@@ -81,8 +93,11 @@ $nodes[] = array(
   'promote' => 0,
   'sticky' => 0,
   'type' => 'portfolio',
-  'body' => '<p>Escape is a free PSD web template built by Mathavan Jaya. Escape is a one page web template that was designed with agencies in mind. This template is ideal for those looking for a simple one page solution to describe your business and offer your services.</p>
+  'body' => [
+    'value' => '<p>Escape is a free PSD web template built by Mathavan Jaya. Escape is a one page web template that was designed with agencies in mind. This template is ideal for those looking for a simple one page solution to describe your business and offer your services.</p>
 <p>You can download the PSD template in this portfolio sample item at FreebiesXpress.com.</p>',
+    'format' => 'basic_html'
+  ],
   'field__subtitle' => 'Lorem ipsum dolor sit amet consectetur.',
   'field_portfolio_screenshot' => $files['portfolio/escape.png'],
   'field_portfolio_date' => 'July 2013',
@@ -98,8 +113,11 @@ $nodes[] = array(
   'promote' => 0,
   'sticky' => 0,
   'type' => 'portfolio',
-  'body' => '<p>Dreams is a free PSD web template built by Mathavan Jaya. Dreams is a modern one page web template designed for almost any purpose. It’s a beautiful template that’s designed with the Bootstrap framework in mind.</p>
+  'body' => [
+    'value' => '<p>Dreams is a free PSD web template built by Mathavan Jaya. Dreams is a modern one page web template designed for almost any purpose. It’s a beautiful template that’s designed with the Bootstrap framework in mind.</p>
 <p>You can download the PSD template in this portfolio sample item at FreebiesXpress.com.</p>',
+    'format' => 'basic_html'
+  ],
   'field__subtitle' => 'Lorem ipsum dolor sit amet consectetur.',
   'field_portfolio_screenshot' => $files['portfolio/dreams.png'],
   'field_portfolio_date' => 'June 2013',
